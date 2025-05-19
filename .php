d84ebc0f991b2587229532c1506ca4e792d67315
Archanadevi711 <archanadevielumalai@gmail.com>
2025-05-19 22:45:21 +0530

    Add files via upload

---
+++ .php
@@ -0,0 +1,15 @@
+<?
+$num=10;
+if($num=<5)
+{
+ echo "the number is less than 5.";
+}
+else if($num>5)
+{
+ echo "the number is greater than 5.";
+}
+else
+{
+ echo "the number is equal to 5.";
+}
+?>
